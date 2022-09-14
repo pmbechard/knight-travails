5b43058eab78017a30f56c010c5234c8ee577294
Peyton Bechard <pbechard@my.yorku.ca>
2022-09-14 22:15:21 +0800

    Cleans up file structure and styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { getBoard } from './components/Board';
-import { getControls } from './components/Controls';
-import { getFooter } from './components/Footer';
 import { getHeader } from './components/Header';
+import { getControls } from './components/Controls';
+import { getBoard } from './components/Board';
+import { getFooter } from './components/Footer';
+
 import './style.css';
 
 getHeader();
