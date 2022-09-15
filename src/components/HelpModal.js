7da5cc73fb824ca95406f9897f1378434b2044ee
Peyton Bechard <pbechard@my.yorku.ca>
2022-09-15 20:09:05 +0800

    Adds text content to help modal

--- src/components/HelpModal.js
+++ src/components/HelpModal.js
@@ -14,4 +14,38 @@ export function getHelpModal() {
   const modalWindow = document.createElement('div');
   modalWindow.classList.add('modal-window');
   container.appendChild(modalWindow);
+
+  const heading = document.createElement('h1');
+  heading.textContent = 'Help!';
+  modalWindow.appendChild(heading);
+
+  const whatHeading = document.createElement('h2');
+  whatHeading.textContent = 'What is this place?';
+  modalWindow.appendChild(whatHeading);
+
+  const whatParagraph = document.createElement('p');
+  whatParagraph.textContent =
+    "This is the visual display of an algorithm that helps our knight friend find the shortest route to the specified location from where ever you've decided to place him. Knights have the ability to reach any place on the chess board. Give it a shot!";
+  modalWindow.appendChild(whatParagraph);
+
+  const howHeading = document.createElement('h2');
+  howHeading.textContent = 'How do I do it?';
+  modalWindow.appendChild(howHeading);
+
+  const howParagraph = document.createElement('p');
+  howParagraph.innerHTML =
+    '1. Choose a spot on the chess board to place our knight. <br> 2. Choose a spot on the chess board to place the destination you want him to reach. <br> 3. Click the <u>Start</u> button to begin! <br><br> Press <u>Restart</u> at any time to clear the board.';
+  modalWindow.appendChild(howParagraph);
+
+  const btnContainer = document.createElement('div');
+  btnContainer.classList.add('try-btn-container');
+  modalWindow.appendChild(btnContainer);
+
+  const tryBtn = document.createElement('button');
+  tryBtn.classList.add('btn');
+  tryBtn.textContent = 'Try it out!';
+  btnContainer.appendChild(tryBtn);
+  tryBtn.addEventListener('click', () => {
+    container.style.display = 'none';
+  });
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import { getHeader } from './components/Header';
 import { getControls } from './components/Controls';
 import { getBoard } from './components/Board';
 import { getFooter } from './components/Footer';
-
-import './style.css';
-import { Marker } from './data/MarkerState';
 import { getHelpModal } from './components/HelpModal';
+
+import { Marker } from './data/MarkerState';
+import './style.css';
 
 export const markerState = new Marker(false);
 
-// FIXME: start btn hover and active properties show when btn is disabled
+// TODO: Add algorithm with tests
+// TODO: Animate algorithm results
+// TODO: Update reset function
+// TODO: Add _MediaQueries.scss
 
 getHeader();
 getControls();
