4fd7e0816f6f961452a7c89475681f9ab831ad6c
Peyton Bechard <pbechard@my.yorku.ca>
2022-09-16 10:13:17 +0800

    Adds extensive tests for getChildren() function in pathfinder

--- src/tests/PathFinder.test.js
+++ src/tests/PathFinder.test.js
@@ -6,6 +6,42 @@ describe('Pathfinder algorithm', () => {
       [1, 2],
       [2, 1],
     ]);
+    expect(getChildren([7, 0])).toStrictEqual([
+      [6, 2],
+      [5, 1],
+    ]);
+    expect(getChildren([0, 7])).toStrictEqual([
+      [2, 6],
+      [1, 5],
+    ]);
+    expect(getChildren([7, 7])).toStrictEqual([
+      [6, 5],
+      [5, 6],
+    ]);
+    expect(getChildren([0, 3])).toStrictEqual([
+      [1, 5],
+      [2, 4],
+      [2, 2],
+      [1, 1],
+    ]);
+    expect(getChildren([3, 0])).toStrictEqual([
+      [2, 2],
+      [4, 2],
+      [5, 1],
+      [1, 1],
+    ]);
+    expect(getChildren([7, 3])).toStrictEqual([
+      [6, 5],
+      [6, 1],
+      [5, 2],
+      [5, 4],
+    ]);
+    expect(getChildren([3, 7])).toStrictEqual([
+      [5, 6],
+      [4, 5],
+      [2, 5],
+      [1, 6],
+    ]);
     expect(getChildren([3, 3])).toStrictEqual([
       [2, 5],
       [4, 5],
